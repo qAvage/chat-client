667526ed24a470cd4e495fc159781aceea01e19f
Leon <57415444+qAvage@users.noreply.github.com>
2025-01-31 01:24:23 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,42 @@
+import './style.css'
+import { io } from "socket.io-client";
+
+const name = prompt('Введите своё имя').trim()
+console.log(import.meta.env.VITE_YRT)
+
+const socket = io('ws://localhost:8000', {
+  withCredentials: true,
+  extraHeaders: {
+    "my-custom-header": "abcd"
+  }
+});
+
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const messages = document.getElementById('messages');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (input.value && input.value.trim() !== '') {
+    socket.emit('chat message', input.value.trim(), name === '' ? 'Amogus' : name)
+    input.value = ''
+  }
+});
+
+socket.on('chat message', (msg, name, time) => {
+  messages.insertAdjacentHTML('beforeend', `
+    <li>
+      <div>
+        <span style="font-weight:700;">${name}:</span>
+        <span>${msg}</span>
+      </div>
+      <div>
+        <span>${time}</span>
+      </div>
+    </li>
+    `)
+  window.scrollTo(0, document.body.scrollHeight);
+});
+
+
+
